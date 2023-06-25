989412ffc9ef0e74347625341d9b7b1b1e3a50b7
nathan shearer <help@rapidstartup.io>
2023-06-25 12:05:20 +0800

    move to public update home controller

--- wave/routes/web.php
+++ wave/routes/web.php
@@ -4,7 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::impersonate();
 
-Route::get('/', '\Wave\Http\Controllers\HomeController@index')->name('wave.home');
+#Route::get('/', '\Wave\Http\Controllers\HomeController@index')->name('wave.home');
+Route::get('/', function () {    return view('index'); });
 Route::get('@{username}', '\Wave\Http\Controllers\ProfileController@index')->name('wave.profile');
 
 // Documentation routes
