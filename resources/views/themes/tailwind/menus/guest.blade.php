75e2ec2531d39f4633873dbe390cc4b8a3211cd6
nathan shearer <help@rapidstartup.io>
2023-07-08 02:51:13 +0800

    add to front guest

--- resources/views/themes/tailwind/menus/guest.blade.php
+++ resources/views/themes/tailwind/menus/guest.blade.php
@@ -20,7 +20,7 @@
                 <div class="flex items-stretch justify-center overflow-hidden bg-white shadow-xs xl:rounded-xl">
 
                     <div class="flex flex-col">
-                        <div class="flex h-full">
+                        <!--<div class="flex h-full">
                             <div class="relative flex flex-col items-start justify-start hidden w-full h-full max-w-xs bg-center bg-cover lg:block" style="background-image:url('https://cdn.devdojo.com/images/october2020/wave-menu-bg.jpeg')">
                                 <div class="relative flex flex-col items-start justify-center w-full h-full px-16 py-8">
 
@@ -149,7 +149,7 @@
                                         </p>
                                     </div>
                                 </a>
-                        </div>
+                        </div>-->
 
                         </div>
 
@@ -227,6 +227,11 @@
             </div>
         </div>
     </div>
+
+    <button id="theme-toggle" type="button" class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5">
+            <svg id="theme-toggle-dark-icon" class="w-5 h-5 hidden" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path></svg>
+            <svg id="theme-toggle-light-icon" class="w-5 h-5 hidden" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+    </button>
 
     <div class="w-1 h-5 mx-10 border-r border-gray-300"></div>
     <a href="{{ route('login') }}" class="text-base font-medium leading-6 text-gray-500 whitespace-no-wrap hover:text-wave-600 focus:outline-none focus:text-gray-900">
