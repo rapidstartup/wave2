d5283c5ad1db73884bf86ac93d44daa97ac9d3bd
nathan shearer <help@rapidstartup.io>
2023-07-08 02:40:25 +0800

    redo dark mode

--- resources/views/themes/tailwind/layouts/app.blade.php
+++ resources/views/themes/tailwind/layouts/app.blade.php
@@ -90,36 +90,40 @@
         @waveCheckout
         <!-- Scripts -->
         <script>
-            // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+            var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+            var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+            // Change the icons inside the button based on previous settings
             if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-                document.documentElement.classList.add('dark');
-                document.getElementById('theme-toggle-light-icon').classList.remove('hidden');
+                themeToggleLightIcon.classList.remove('hidden');
             } else {
-                document.documentElement.classList.remove('dark');
-                document.getElementById('theme-toggle-dark-icon').classList.remove('hidden');
+                themeToggleDarkIcon.classList.remove('hidden');
             }
-        
             var themeToggleBtn = document.getElementById('theme-toggle');
-        
             themeToggleBtn.addEventListener('click', function() {
-                var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
-                var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
-        
                 // toggle icons inside button
                 themeToggleDarkIcon.classList.toggle('hidden');
                 themeToggleLightIcon.classList.toggle('hidden');
-        
-                // toggle dark mode class on the HTML element
-                document.documentElement.classList.toggle('dark');
-        
-                // update the color theme setting in local storage
-                if (document.documentElement.classList.contains('dark')) {
-                    localStorage.setItem('color-theme', 'dark');
+                // if set via local storage previously
+                if (localStorage.getItem('color-theme')) {
+                    if (localStorage.getItem('color-theme') === 'light') {
+                        document.documentElement.classList.add('dark');
+                        localStorage.setItem('color-theme', 'dark');
+                    } else {
+                        document.documentElement.classList.remove('dark');
+                        localStorage.setItem('color-theme', 'light');
+                    }
+                // if NOT set via local storage previously
                 } else {
-                    localStorage.setItem('color-theme', 'light');
+                    if (document.documentElement.classList.contains('dark')) {
+                        document.documentElement.classList.remove('dark');
+                        localStorage.setItem('color-theme', 'light');
+                    } else {
+                        document.documentElement.classList.add('dark');
+                        localStorage.setItem('color-theme', 'dark');
+                    }
                 }
             });
         </script>
-        
+
     </body>
     </html>
