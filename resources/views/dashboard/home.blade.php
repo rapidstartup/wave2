61e356dec89de9cc85b3f03c69485e6da19d5a33
Logan Oh <logansanoh@gmail.com>
2023-08-10 16:17:14 +0000

    code optimizing

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -5,10 +5,9 @@
 @section('content')
 
     <!-- Resources -->
-    <script src="https://cdn.amcharts.com/lib/5/index.js"></script>
-    <script src="https://cdn.amcharts.com/lib/5/xy.js"></script>
-    <script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>
-
+{{--    <script src="https://cdn.amcharts.com/lib/5/index.js"></script>--}}
+{{--    <script src="https://cdn.amcharts.com/lib/5/xy.js"></script>--}}
+{{--    <script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>--}}
 
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
